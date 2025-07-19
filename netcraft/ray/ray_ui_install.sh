1c8cf6d4bf1ab73b20bae364e6494cca60ed7fdd
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-07-19 22:36:09 +0800

    Update ray_ui_install.sh

--- netcraft/ray/ray_ui_install.sh
+++ netcraft/ray/ray_ui_install.sh
@@ -42,6 +42,7 @@ rm ./$ui_dir/bin/README.md
 chmod +x ./$ui_dir/ray-ui
 
 echo "Hello, it works fine and the tips for running are below."
-echo "Caution: please execute [cd ./ray_ui_assets] before running ray-ui"
+echo "Caution: please execute [cd ./ray_ui_assets] before running ray-ui,"
+echo "if you have no write permission in /etc or /var, please execute [export XUI_DB_FOLDER=./ && export XUI_LOG_FOLDER=./]"
 echo "CMD: ./ray_ui_assets/ray-ui"
 echo "Params: run, setting[-username,-password,-port,-webBasePath]"
