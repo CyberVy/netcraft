d988e11649424ae9c4229c4591792cc0f355ac32
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-07-30 01:47:38 +0800

    Update cfd_install.sh

--- netcraft/cloudflared/cfd_install.sh
+++ netcraft/cloudflared/cfd_install.sh
@@ -6,27 +6,29 @@ echo "$sys_info"
 arch_info=$(uname -m)
 echo "$arch_info"
 
+version="2025.7.0"
+
 if [[ "$sys_info" = "Darwin" &&  ("$arch_info" = "x86_64"  ||  "$arch_info" = "amd64") ]]; then
-  curl -Lo ./cloudflared.tgz "https://github.com/cloudflare/cloudflared/releases/download/2025.7.0/cloudflared-darwin-amd64.tgz"
+  curl -Lo ./cloudflared.tgz "https://github.com/cloudflare/cloudflared/releases/download/$version/cloudflared-darwin-amd64.tgz"
   tar -xzf ./cloudflared.tgz
   rm ./cloudflared.tgz
 
 elif [[ "$sys_info" = "Darwin" &&  ("$arch_info" = "arm64" || "$arch_info" = "aarch64") ]]; then
-  curl -Lo ./cloudflared.tgz "https://github.com/cloudflare/cloudflared/releases/download/2025.7.0/cloudflared-darwin-arm64.tgz"
+  curl -Lo ./cloudflared.tgz "https://github.com/cloudflare/cloudflared/releases/download/$version/cloudflared-darwin-arm64.tgz"
   tar -xzf ./cloudflared.tgz
   rm ./cloudflared.tgz
 
 elif [[ "$sys_info" = "FreeBSD" &&  ("$arch_info" = "x86_64"  ||  "$arch_info" = "amd64") ]]; then
-  curl -Lo ./cloudflared "https://github.com/cloudflare/cloudflared/releases/download/2025.7.0/cloudflared-amd64.pkg"
+  curl -Lo ./cloudflared "https://github.com/cloudflare/cloudflared/releases/download/$version/cloudflared-amd64.pkg"
 
 elif [[ "$sys_info" = "FreeBSD" &&  ("$arch_info" = "arm64" || "$arch_info" = "aarch64") ]]; then
-    curl -Lo ./cloudflared "https://github.com/cloudflare/cloudflared/releases/download/2025.7.0/cloudflared-arm64.pkg"
+  curl -Lo ./cloudflared "https://github.com/cloudflare/cloudflared/releases/download/$version/cloudflared-arm64.pkg"
 
 elif [[ "$sys_info" = "Linux" &&  ("$arch_info" = "x86_64"  ||  "$arch_info" = "amd64") ]]; then
-  curl -Lo ./cloudflared "https://github.com/cloudflare/cloudflared/releases/download/2025.7.0/cloudflared-linux-amd64"
+  curl -Lo ./cloudflared "https://github.com/cloudflare/cloudflared/releases/download/$version/cloudflared-linux-amd64"
 
 elif [[ "$sys_info" = "Linux" &&  ("$arch_info" = "arm64" || "$arch_info" = "aarch64") ]]; then
-  curl -Lo ./cloudflared "https://github.com/cloudflare/cloudflared/releases/download/2025.7.0/cloudflared-linux-arm64"
+  curl -Lo ./cloudflared "https://github.com/cloudflare/cloudflared/releases/download/$version/cloudflared-linux-arm64"
 
 else
   echo "Not Supported OS ar arch."
