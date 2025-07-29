d76c64a857679cbc30d2e4b908e2e8d2fca1c6bc
CyberVy <72680847+CyberVy@users.noreply.github.com>
2025-07-30 01:48:52 +0800

    Update ray_ui_install.sh

--- netcraft/ray/ray_ui_install.sh
+++ netcraft/ray/ray_ui_install.sh
@@ -6,10 +6,11 @@ echo "$sys_info"
 arch_info=$(uname -m)
 echo "$arch_info"
 
+version="v2.6.2"
 ui_dir="ray_ui_assets"
 
 if [[ "$sys_info" = "Linux" &&  ("$arch_info" = "x86_64"  ||  "$arch_info" = "amd64") ]]; then
-  curl -Lo ./_linux-64.tar.gz "https://github.com/MHSanaei/3x-ui/releases/download/v2.6.2/x-ui-linux-amd64.tar.gz"
+  curl -Lo ./_linux-64.tar.gz "https://github.com/MHSanaei/3x-ui/releases/download/$version/x-ui-linux-amd64.tar.gz"
 
   tar -xzf _linux-64.tar.gz
   mv ./x-ui ./$ui_dir
@@ -21,7 +22,7 @@ if [[ "$sys_info" = "Linux" &&  ("$arch_info" = "x86_64"  ||  "$arch_info" = "am
   chmod +x ./$ui_dir/bin/xray-linux-amd64
 
 elif [[ "$sys_info" = "Linux" &&  ("$arch_info" = "arm64" || "$arch_info" = "aarch64") ]]; then
-  curl -Lo ./_linux-64.tar.gz "https://github.com/MHSanaei/3x-ui/releases/download/v2.6.2/x-ui-linux-arm64.tar.gz"
+  curl -Lo ./_linux-64.tar.gz "https://github.com/MHSanaei/3x-ui/releases/download/$version/x-ui-linux-arm64.tar.gz"
 
   tar -xzf _linux-64.tar.gz
   mv ./x-ui ./$ui_dir
